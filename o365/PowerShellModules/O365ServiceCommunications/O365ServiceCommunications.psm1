8e6e0a00d423713508798bb2dfab7149ffcc5ce0
chrisdee <christopherdee@hotmail.com>
2018-03-05 18:44:57 +0100

    Added O365 PowerShell Modules

---
+++ o365/PowerShellModules/O365ServiceCommunications/O365ServiceCommunications.psm1
@@ -0,0 +1,7 @@
+$Functions = Get-ChildItem -Path "$PSScriptRoot\functions" -File -Filter *.ps1
+$Helpers = Get-ChildItem -Path "$PSScriptRoot\helpers" -File -Filter *.ps1
+
+foreach ($Script in ($Functions + $Helpers))
+{
+    . $Script.FullName
+}
---
+++ o365/PowerShellModules/O365ServiceCommunications/helpers/Config.ps1
@@ -0,0 +1,7 @@
+$ServiceUrl = 'https://api.admin.microsoftonline.com/shdtenantcommunications.svc'
+
+$SessionTypeName = 'O365ServiceCommunications.Session'
+$EventTypeName   = 'O365ServiceCommunications.Event'
+$TenantEventTypeName = 'O365ServiceCommunications.TenantEvent'
+$TenantServiceInfoTypeName = 'O365ServiceCommunications.TenantServiceInfo'
+$ServiceInfoTypeName   = 'O365ServiceCommunications.ServiceInfo'
